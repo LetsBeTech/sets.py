6a378752669dfd96093b50fdcccb04682f5dfeb7
TechnoCodin <86043165+TechnoCodin@users.noreply.github.com>
2021-06-30 13:21:25 +0530

    Sets

---
+++ set-basics.py
@@ -0,0 +1,16 @@
+#Syntax of sets
+
+MySet = {1,3,5,7,8,13,4}
+
+#printing the whole set
+print(MySet)
+
+#Methods related to sets
+
+MySet = MySet.pop()#Removes any random element from the list
+
+MySet = MySet.clear()#Empties the set
+
+MySet = MySet.discard(3)#Will discard 3 if present
+
+
